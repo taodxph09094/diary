1f20d135cec2b6af4500a910d1b91fccf8e1135c
taodxph09094 <taodxph09094@gmail.com>
2023-12-01 14:00:59 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Home from "./pages/Home";  
 import ForMe from "./pages/ForMe";
 import Public from "./pages/Public";
 import Detail from "./pages/Detail";
-import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import CreatePage from "./pages/Post/CreatePage";
 import { jwtDecode } from "jwt-decode";
-
+import { Analytics } from '@vercel/analytics/react';
 function App() {
   const navigate = useNavigate();
 
   const userToken = localStorage.getItem("userToken");
 
-  const decodedToken = jwtDecode(userToken);
+
+ 
 
   useEffect(() => {
     if (!userToken) {
       navigate("/");
       return;
     }
-    if (decodedToken) {
-      const expirationTime = decodedToken.exp;
-      const currentTime = Math.floor(Date.now() / 1000);
-      const timeRemaining = expirationTime - currentTime;
-      console.log("Thời gian còn lại (giây):", timeRemaining);
-      const minutesRemaining = Math.floor(timeRemaining / 60);
-      console.log("Thời gian còn lại (phút):", minutesRemaining);
-      if (timeRemaining <= 0) {
-        console.log("Token đã hết hạn, chuyển hướng về trang chủ...");
+    if(userToken){
+      const decodedToken = jwtDecode(userToken);
+      if (decodedToken) {
+        const expirationTime = decodedToken.exp;
+        const currentTime = Math.floor(Date.now() / 1000);
+        const timeRemaining = expirationTime - currentTime;
+        console.log("Thời gian còn lại (giây):", timeRemaining);
+        const minutesRemaining = Math.floor(timeRemaining / 60);
+        console.log("Thời gian còn lại (phút):", minutesRemaining);
+        if (timeRemaining <= 0) {
+          console.log("Token đã hết hạn, chuyển hướng về trang chủ...");
+          localStorage.clear();
+          navigate("/");
+        } else {
+          console.log("Token còn hạn, tiếp tục xử lý...");
+        }
+      } else {
+        console.error("Không thể giải mã token:", decodedToken);
         localStorage.clear();
         navigate("/");
-      } else {
-        console.log("Token còn hạn, tiếp tục xử lý...");
       }
-    } else {
-      console.error("Không thể giải mã token:", decodedToken);
-      localStorage.clear();
-      navigate("/");
     }
-  }, [decodedToken, userToken]);
+   
+  }, [ userToken]);
 
   return (
     <>
@@ -57,6 +60,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       )}
+       <Analytics />
     </>
   );
 }
